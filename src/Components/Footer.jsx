d79eaadca71c2d43d2ecc543441255ba78c40028
Prasad <prasadmhaske2001@gmail.com>
2023-09-03 22:29:13 +0530

    done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
   return (
     <footer className="bg-dark text-light py-4">
       <Container>
-        <Row>
-          <Col md={6}>
-            <h4>Contact Us</h4>
-            <p>123 Main St, City, Country</p>
-            <p>Email: info@example.com</p>
-            <p>Phone: +1 123-456-7890</p>
+        <Row className="g-4">
+          <Col md={4}>
+            <h4>Contact Me</h4>
+            <p>Kopergaon, Maharashatra, India</p>
+            <p>Email: prasadmhaske2001@gmail.com</p>
+            <p>Phone: 9764584028</p>
           </Col>
-          <Col md={6}>
+          <Col md={4}>
+            <h4>About Us</h4>
+            <p>Know more</p>
+            <p>Search here</p>
+            <p>Find more</p>
+          </Col>
+          <Col md={4}>
             <h4>Copyright &copy; {new Date().getFullYear()}</h4>
             <p>All Rights Reserved</p>
           </Col>
